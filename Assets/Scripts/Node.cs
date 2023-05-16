c64768344d032b373d9a824125fab193b457cbce
Shelton Thomas <sheltonjthomasjr@gmail.com>
2023-05-16 18:20:00 -0500

    Reduced Movement code, added moving to nodes

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    private Node[] connectedNodes = new Node[0];
+
+    public Node[] GetNodes() 
+    {
+        return connectedNodes; 
+    }
+
+    public void AddNode(Node newNode)
+    {
+        Node[] tempConnectedNodes = connectedNodes;
+        connectedNodes = new Node[connectedNodes.Length + 1];
+        
+        for(int i = 0; i < connectedNodes.Length; i++)
+        {
+            connectedNodes[i] = tempConnectedNodes[i];
+        }
+        connectedNodes[connectedNodes.Length - 1] = newNode;
+    }
+
+    public Vector3 Position { get { return transform.position; } }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,117 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     private IEnumerator _movementCoroutine;
-    private Quaternion startingRotation;
     public float speed = 1;
     private bool _isMoving = false;
-    public float hitboxSize = 1.5f;
-    public Transform destination;
-    private Vector3 _destination;
 
+    [SerializeField]
+    private Node _currentNode;
+    private Node[] _currentConnectedNodes;
+    private bool _isMovementCoroutineStopped = true;
+    private bool _needToUpdateMovementCoroutine = false;
     void Start()
     {
-        _destination = destination.position;
+        _movementCoroutine = MoveToDestination(_currentNode.Position);
+        _currentConnectedNodes = _currentNode.GetNodes();
     }
 
     void Update()
     {
-        //if (!_moving)
-        //{
-        //    if(Input.GetAxis("Horizontal") > 0)
-        //    {
-        //        _moving = true;
-        //        destination = transform.position + (Vector3.right * 2);
-        //        _rotationCoroutine = Rotate(new Vector3(0, 0, -90));
-        //        StartCoroutine(_rotationCoroutine);
-        //    }
-        //    else if(Input.GetAxis("Horizontal") < 0)
-        //    {
-        //        _moving = true;
-        //        destination = transform.position + (Vector3.left * 2);
-        //        _rotationCoroutine = Rotate(new Vector3(0, 0, 90));
-        //        StartCoroutine(_rotationCoroutine);
-        //    }
-        //    else if(Input.GetAxis("Vertical") > 0)
-        //    {
-        //        _moving = true;
-        //        destination = transform.position + (Vector3.forward * 2);
-        //        _rotationCoroutine = Rotate(new Vector3(90, 0, 0));
-        //        StartCoroutine(_rotationCoroutine);
-        //    }
-        //    else if(Input.GetAxis("Vertical") < 0)
-        //    {
-        //        _moving = true;
-        //        destination = transform.position + (Vector3.back * 2);
-        //        _rotationCoroutine = Rotate(new Vector3(-90, 0, 0));
-        //        StartCoroutine(_rotationCoroutine);
-        //    }
-        //}
-        if (Input.GetAxis("Vertical") < 0 && !_isMoving)
-        {
-            _isMoving = true;
-            _movementCoroutine = StepToDestination(_destination);
-            StartCoroutine(_movementCoroutine);
-        }
+        if (Input.GetAxis("Vertical") < 0)  _isMoving = true;
+
+        if (_isMoving && _isMovementCoroutineStopped) StartCoroutine(_movementCoroutine);
+        if(_needToUpdateMovementCoroutine) UpdateMovementCoroutine();
     }
 
-    //Following 2 coroutines would not work for movement on the y vector
-
-    //Finishes movement in multiple 90 degree turns
-    IEnumerator StepToDestination(Vector3 destintion, float stepSize = 1)
+    //Finishes movement in either a single 90 degree turn or multiple 90 degree turns
+    IEnumerator MoveToDestination(Vector3 destintion, float stepSize = 1f)
     {
-        Vector3 movementDirection = (destintion - transform.position).normalized * stepSize;
+        Vector3 movementDirection;
+        Vector3 rotationDirection;
         bool isFinishedMoving = false;
-        Vector3 rotationDirection = movementDirection * 90;
-        rotationDirection = new Vector3(rotationDirection.z, rotationDirection.y, -rotationDirection.x);
+        _isMovementCoroutineStopped = false;
 
-        while (true)
+        //Multiple 90 degree turns
+        if (stepSize != 0)
         {
-            transform.Rotate(rotationDirection * Time.deltaTime * speed);
-
-            transform.position += movementDirection * Time.deltaTime * speed;
-            if((transform.position - destintion).magnitude < .01f)
-                isFinishedMoving = true;
-
-            if (isFinishedMoving)
-            {
-                transform.position = destintion;
-                Quaternion newRotation = new Quaternion(Mathf.Round(transform.rotation.x), Mathf.Round(transform.rotation.y), Mathf.Round(transform.rotation.z), Mathf.Round(transform.rotation.w));
-                transform.rotation = newRotation;
-                Debug.Log("Finished");
-                break;
-            }
-            yield return null;
+            movementDirection = (destintion - transform.position).normalized * stepSize;
+            rotationDirection = movementDirection * 90;
         }
-
-        //if (Physics.Raycast(transform.position, Vector3.up, out RaycastHit hit, 1))
-        //{
-        //    FaceBehavior face = hit.collider.gameObject.GetComponent<FaceBehavior>();
-        //}
-        _isMoving = false;
-        StopCoroutine(_movementCoroutine);
-    }
-
-    //Finishes movement in a single 90 degree turn
-    IEnumerator MoveToDestination(Vector3 destintion)
-    {
-        Vector3 movementDirection = (destintion - transform.position);
-        bool isFinishedMoving = false;
-        Vector3 rotationDirection = movementDirection.normalized * 90;
+        //Single 90 degree turn
+        else
+        {
+            movementDirection = destintion - transform.position;
+            rotationDirection = movementDirection.normalized * 90;
+        }
         rotationDirection = new Vector3(rotationDirection.z, rotationDirection.y, -rotationDirection.x);
 
-        while (true)
+        while (!isFinishedMoving)
         {
             transform.Rotate(rotationDirection * Time.deltaTime * speed);
 
             
             transform.position += movementDirection * Time.deltaTime * speed;
-            if ((transform.position - destintion).magnitude < speed * .1f)
+            if ((transform.position - destintion).magnitude < speed * .01f)
                 isFinishedMoving = true;
 
             if (isFinishedMoving)
@@ -119,7 +66,6 @@ public class PlayerMovement : MonoBehaviour
                 transform.position = destintion;
                 Quaternion newRotation = new Quaternion(Mathf.Round(transform.rotation.x), Mathf.Round(transform.rotation.y), Mathf.Round(transform.rotation.z), Mathf.Round(transform.rotation.w));
                 transform.rotation = newRotation;
-                Debug.Log("Finished");
                 break;
             }
             yield return null;
@@ -129,7 +75,19 @@ public class PlayerMovement : MonoBehaviour
         //{
         //    FaceBehavior face = hit.collider.gameObject.GetComponent<FaceBehavior>();
         //}
+        
+        _isMovementCoroutineStopped = true;
         _isMoving = false;
+        _needToUpdateMovementCoroutine = true;
         StopCoroutine(_movementCoroutine);
+    }
+
+    private void UpdateMovementCoroutine()
+    {
+        _needToUpdateMovementCoroutine = false;
+        if (_currentConnectedNodes == null) return;
+        _currentNode = _currentConnectedNodes[0];
+        _currentConnectedNodes = _currentNode.GetNodes();
+        _movementCoroutine = MoveToDestination(_currentNode.gameObject.transform.position);
     }
 }
